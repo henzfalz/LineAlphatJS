0c8aada711b8ada2abcee090b9cd8c344e7dec37
henzfalz <30608496+henzfalz@users.noreply.github.com>
2018-05-05 13:07:03 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ class LINE extends Command {
 
 
     get myBot() {
-        const bot = ['u1cab5ef6299af4713353b9843479952d','u22d94aac4e1659eb6f375ffc7cb17a53'];
+        const bot = ['ueb4d1692060c29f4545aff3d186390ca'];
         return bot; 
     }
 
